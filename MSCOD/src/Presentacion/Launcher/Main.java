2cfff6d76bdc174f34cda2a2c2cdf7316a8181df
Victor Ortego <ortegovictor4@gmail.com>
2023-08-17 19:15:27 +0200

    Proyecto

---
+++ MSCOD/src/Presentacion/Launcher/Main.java
@@ -0,0 +1,13 @@
+package Presentacion.Launcher;
+
+
+import Presentacion.Controller.Controller;
+import Presentacion.Controller.Events;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Controller.obtenerInstancia().accion(Events.ABRIR_VPRINCIPAL, null);
+		
+	}
+}
